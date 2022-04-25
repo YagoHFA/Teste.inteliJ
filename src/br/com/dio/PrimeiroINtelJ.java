b26709847f65fd5892736f9a877d8b86ebdd76fb
YagoHF <yagof2421@gmail.com>
2022-04-25 18:09:08 -0300

    First commit Intel

---
+++ src/br/com/dio/PrimeiroINtelJ.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model1.Gato;
+
+public class PrimeiroINtelJ {
+
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+    }
+}
+
+
