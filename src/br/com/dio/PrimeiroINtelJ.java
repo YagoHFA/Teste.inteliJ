96c9a2c6d6f468626a061a8b7f2aa9fe44009d4f
YagoHF <yagof2421@gmail.com>
2022-04-25 18:37:26 -0300

    criando class livro

--- src/br/com/dio/PrimeiroINtelJ.java
+++ src/br/com/dio/PrimeiroINtelJ.java
@@ -4,11 +4,46 @@ import br.com.dio.model1.Gato;
 
 public class PrimeiroINtelJ {
 
-    public static void main(String[] args) {
+    public static <Livro1> void main(String[] args) {
         Gato gato = new Gato();
 
         System.out.println(gato);
 
+        Object livro1 = new livro1( "O problema dos três corpos", 300);
+        System.out.println(livro1);
+    }
+}
+class livro1 {
+    private String nome;
+    private String numPaginas;
+
+    public livro1(String nome, int numPaginas) {
+        this.nome = nome;
+        this.numPaginas = String.valueOf(numPaginas);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(String numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro1{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas='" + numPaginas + '\'' +
+                '}';
     }
 }
 
---
+++ src/br/com/dio/model1/Livro1.java
@@ -0,0 +1,4 @@
+package br.com.dio.model1;
+
+public class Livro1 {
+}
